097166cf7d327a675da7b7fb8ba5100702488354
Sam Westrick <shwestrick@gmail.com>
2023-03-29 16:53:22 -0400

    update java tokenize and dedup

--- java/Tokenize.java
+++ java/Tokenize.java
@@ -10,6 +10,41 @@ class Tokenize {
     public int length;
   }
 
+  static class TokenGenerator {
+    public String data;
+    public int[] offsets;
+
+    public int numTokens() {
+      return this.offsets.length / 2;
+    }
+
+    public String generateToken(int i) {
+      int start = this.offsets[2*i];
+      int stop = this.offsets[2*i+1];
+      return this.data.substring(start, stop);
+    }
+  }
+
+  static TokenGenerator tokenGenerator(String input) {
+    int n = input.length();
+    int[] ids = IntStream.range(0,n+1).parallel().filter(i -> {
+      if (i == n) {
+        return !(Character.isSpace(input.charAt(n-1)));
+      }
+      else if (i == 0) {
+        return !(Character.isSpace(input.charAt(0)));
+      }
+      boolean i1 = Character.isSpace(input.charAt(i));
+      boolean i2 = Character.isSpace(input.charAt(i-1));
+      return (i1 && !i2) || (i2 && !i1);
+    }).toArray();
+
+    TokenGenerator result = new TokenGenerator();
+    result.data = input;
+    result.offsets = ids;
+    return result;
+  }
+
   static StringSlice[] tokens(String input) {
     int n = input.length();
     int[] ids = IntStream.range(0,n+1).parallel().filter(i -> {
