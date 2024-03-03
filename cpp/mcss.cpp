5776eb199ec18f3e76d93cefa3fe35f9a970f59c
Sam Westrick <shwestrick@gmail.com>
2024-03-03 12:01:50 -0500

    c++ mcss try out no grain control, and bugfix (missing max with 0 for singletons)

--- cpp/mcss.cpp
+++ cpp/mcss.cpp
@@ -8,10 +8,19 @@ int main(int argc, char** argv) {
     return (double)((int)(parlay::hash64(i) % 1000) - 500) / 500.0;
   });
   double result;
+
+  std::cout << std::endl << "WITH GRANULARITY CONTROL" << std::endl;
   pbbsBench::launch([&] {
     result = mcss_delayed_reduce(A);
   });
   std::cout << "result " << result << std::endl;
+
+  std::cout << std::endl << "NO GRANULARITY CONTROL" << std::endl;
+  pbbsBench::launch([&] {
+    result = mcss_no_gran(A);
+  });
+  std::cout << "result " << result << std::endl;
+  
   return 0;
 }
 
--- cpp/mcss.h
+++ cpp/mcss.h
@@ -16,10 +16,53 @@ auto mcss_delayed_reduce(parlay::sequence<double> const& A) {
   double neginf = std::numeric_limits<double>::lowest();
   tu identity = {neginf, neginf, neginf, (double) 0.0};
   auto pre = parlay::delayed_seq<tu>(A.size(), [&] (size_t i) -> tu {
-      tu x = {A[i],A[i],A[i],A[i]};
+      double v = A[i];
+      double vp = std::max(0.0, v);
+      tu x = {vp, vp, vp, v};
       return x;
     });
   auto r = parlay::reduce(pre, parlay::make_monoid(f, identity));
   // t.next("reduce");
+  return r[0];
+}
+
+
+auto mcss_no_gran_recursive(parlay::sequence<double> const& A, long lo, long hi) {
+  using tu = std::array<double,4>;
+
+  double neginf = std::numeric_limits<double>::lowest();
+
+  if (lo >= hi) {
+    tu result = {neginf, neginf, neginf, (double)0.0};
+    return result;
+  }
+
+  if (lo+1 == hi) {
+    double v = A[lo];
+    double vp = std::max(0.0, v);
+    tu result = {vp, vp, vp, v};
+    return result;
+  }
+
+  long mid = lo + (hi-lo)/2;
+  tu l, r;
+  parlay::par_do(
+    [&] { l = mcss_no_gran_recursive(A, lo, mid); },
+    [&] { r = mcss_no_gran_recursive(A, mid, hi); }
+  );
+
+  auto f = [] (tu a, tu b) {
+    tu r = {std::max(std::max(a[0],b[0]),a[2]+b[1]),
+	    std::max(a[1],a[3]+b[1]),
+	    std::max(a[2]+b[3],b[2]),
+	    a[3]+b[3]};
+    return r;};
+
+  return f(l, r);
+}
+
+auto mcss_no_gran(parlay::sequence<double> const& A) {
+  using tu = std::array<double,4>;
+  tu r = mcss_no_gran_recursive(A, 0, A.size());
   return r[0];
 }
